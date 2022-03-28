d2e2fa9f80880bd7b660270dae361d27c3232aa6
Vasco Sousa <myemailvasco@gmail.com>
2022-03-28 20:15:51 +0100

    Improved sequential version

--- prob1/prob1-seq.c
+++ prob1/prob1-seq.c
@@ -3,114 +3,109 @@
 #include <string.h>
 #include <time.h>
 
-int are_utf8_chars_equal(char *c1, char *c2)
+int is_vowel(int c)
 {
-    int i = 0;
-
-    while (i < 4)
-    {
-        if (*(c1 + i) != *(c2 + i) && *(c1 + i) != 0 && *(c2 + i) != 0)
-        {
-            return 0;
-        }
-
-        i++;
-    }
-
-    return 1;
-}
-
-int is_vowel(char *c)
-{
-    char ac_vowels[40][4] = {
-        {0x61, 0x00, 0x00, 0x00},
-        {0x65, 0x00, 0x00, 0x00},
-        {0x69, 0x00, 0x00, 0x00},
-        {0x6F, 0x00, 0x00, 0x00},
-        {0x75, 0x00, 0x00, 0x00},
-        {0x41, 0x00, 0x00, 0x00},
-        {0x45, 0x00, 0x00, 0x00},
-        {0x49, 0x00, 0x00, 0x00},
-        {0x4F, 0x00, 0x00, 0x00},
-        {0x55, 0x00, 0x00, 0x00},
-        {0xC3, 0xA1, 0x00, 0x00},
-        {0xC3, 0xA0, 0x00, 0x00},
-        {0xC3, 0xA2, 0x00, 0x00},
-        {0xC3, 0xA3, 0x00, 0x00},
-        {0xC3, 0xA9, 0x00, 0x00},
-        {0xC3, 0xA8, 0x00, 0x00},
-        {0xC3, 0xAA, 0x00, 0x00},
-        {0xC3, 0xAD, 0x00, 0x00},
-        {0xC3, 0xAC, 0x00, 0x00},
-        {0xC3, 0xB3, 0x00, 0x00},
-        {0xC3, 0xB2, 0x00, 0x00},
-        {0xC3, 0xB4, 0x00, 0x00},
-        {0xC3, 0xB5, 0x00, 0x00},
-        {0xC3, 0xBA, 0x00, 0x00},
-        {0xC3, 0xB9, 0x00, 0x00},
-        {0xC3, 0x81, 0x00, 0x00},
-        {0xC3, 0x80, 0x00, 0x00},
-        {0xC3, 0x82, 0x00, 0x00},
-        {0xC3, 0x83, 0x00, 0x00},
-        {0xC3, 0x89, 0x00, 0x00},
-        {0xC3, 0x88, 0x00, 0x00},
-        {0xC3, 0x8A, 0x00, 0x00},
-        {0xC3, 0x8D, 0x00, 0x00},
-        {0xC3, 0x8C, 0x00, 0x00},
-        {0xC3, 0x93, 0x00, 0x00},
-        {0xC3, 0x92, 0x00, 0x00},
-        {0xC3, 0x94, 0x00, 0x00},
-        {0xC3, 0x95, 0x00, 0x00},
-        {0xC3, 0x9A, 0x00, 0x00},
-        {0xC3, 0x99, 0x00, 0x00}};
+    int ac_vowels[40] = {
+        0x61,
+        0x65,
+        0x69,
+        0x6F,
+        0x75,
+        0x41,
+        0x45,
+        0x49,
+        0x4F,
+        0x55,
+        0xC3A1,
+        0xC3A0,
+        0xC3A2,
+        0xC3A3,
+        0xC3A9,
+        0xC3A8,
+        0xC3AA,
+        0xC3AD,
+        0xC3AC,
+        0xC3B3,
+        0xC3B2,
+        0xC3B4,
+        0xC3B5,
+        0xC3BA,
+        0xC3B9,
+        0xC381,
+        0xC380,
+        0xC382,
+        0xC383,
+        0xC389,
+        0xC388,
+        0xC38A,
+        0xC38D,
+        0xC38C,
+        0xC393,
+        0xC392,
+        0xC394,
+        0xC395,
+        0xC39A,
+        0xC399};
 
     for (int i = 0; i < 40; i++)
     {
-        if (are_utf8_chars_equal(c, ac_vowels[i]))
+        if (c == ac_vowels[i])
+        {
             return 1;
+        }
     }
 
     return 0;
 }
 
-int is_consonant(char *c)
+int is_consonant(unsigned int c)
 {
-    char lower_cedilha[4] = {0xC3, 0xA7, 0x00, 0x00};
-    char upper_cedilha[4] = {0xC3, 0x87, 0x00, 0x00};
-    return !is_vowel(c) && (((*c >= 65 && *c <= 90) || (*c >= 97 && *c <= 122)) || are_utf8_chars_equal(c, lower_cedilha) || are_utf8_chars_equal(c, upper_cedilha));
+    int lower_cedilha = 0xC3A7;
+    int upper_cedilha = 0xC387;
+    return !is_vowel(c) && (((c >= 65 && c <= 90) || (c >= 97 && c <= 122)) || c == lower_cedilha || c == upper_cedilha);
 }
 
-int is_separator(char *c)
+int is_separator(unsigned int c)
 {
-    char dash[4] = {0xE2, 0x80, 0x93, 0x00};
-    char ellipsis[4] = {0xE2, 0x80, 0xA6, 0x00};
-    char doubleQuotationMarkLeft[4] = {0xE2, 0x80, 0x9C, 0x00};
-    char doubleQuotationMarkRight[4] = {0xE2, 0x80, 0x9D, 0x00};
-    return *c == '[' || *c == ']' || *c == '(' || *c == ')' || *c == '-' || *c == '"' || *c == 0x20 || *c == 0x9 || *c == 0xA || *c == 0xD || *c == '.' || *c == '?' || *c == ';' || *c == ':' || *c == ',' || *c == '!' || are_utf8_chars_equal(c, dash) || are_utf8_chars_equal(c, ellipsis) || are_utf8_chars_equal(c, doubleQuotationMarkLeft) || are_utf8_chars_equal(c, doubleQuotationMarkRight);
+    int dash = 0xE28093;
+    int ellipsis = 0xE280A6;
+    int doubleQuotationMarkLeft = 0xE2809C;
+    int doubleQuotationMarkRight = 0xE2809D;
+    return c == '[' || c == ']' || c == '(' || c == ')' || c == '-' || c == '"' || c == 0x20 || c == 0x9 || c == 0xA || c == 0xD || c == '.' || c == '?' || c == ';' || c == ':' || c == ',' || c == '!' || c == dash || c == ellipsis || c == doubleQuotationMarkLeft || c == doubleQuotationMarkRight;
 }
 
-int is_number(char *c)
+int is_number(unsigned int c)
 {
-    return *c >= 48 && *c <= 57;
+    return c >= 48 && c <= 57;
 }
 
-char *read_utf8_char(FILE *file)
+int read_utf8_char(FILE *file)
 {
-    char *buffer = malloc(sizeof(char) * 4);
-    buffer[0] = 0;
+    unsigned char buffer[4] = {0, 0, 0, 0};
+    unsigned int c = 0;
     fread(buffer, 1, 1, file);
-    buffer[1] = 0;
-    buffer[2] = 0;
-    buffer[3] = 0;
+    c = buffer[0];
 
-    if (buffer[0] >= -64 && buffer[0] <= -33)
-        fread(buffer + 1, 1, 1, file);
-    else if (buffer[0] >= -32 && buffer[0] <= -17)
-        fread(buffer + 1, 1, 2, file);
-    else if (buffer[0] >= -16 && buffer[0] <= -9)
-        fread(buffer + 1, 1, 3, file);
+    if ((buffer[0] >> 5) == 0b110)
+    {
+        fread(&buffer[1], 1, 1, file);
+        c = (c << 8) | (buffer[1] & 0xff);
+    }
+    else if ((buffer[0] >> 4) == 0b1110)
+    {
+        fread(&buffer[1], 1, 2, file);
+        c = (c << 8) | (buffer[1] & 0xff);
+        c = (c << 8) | (buffer[2] & 0xff);
+    }
+    else if ((buffer[0] >> 3) == 0b11110)
+    {
+        fread(&buffer[1], 1, 3, file);
+        c = (c << 8) | (buffer[1] & 0xff);
+        c = (c << 8) | (buffer[2] & 0xff);
+        c = (c << 8) | (buffer[2] & 0xff);
+    }
 
-    return buffer;
+    return c;
 }
 
 void process_file(const char *file_path)
@@ -127,13 +122,13 @@ void process_file(const char *file_path)
         exit(-1);
     }
 
-    char *last_c = malloc(sizeof(char) * 4);
+    int last_c = 0;
 
     while (1)
     {
-        char *c = read_utf8_char(file);
+        int c = read_utf8_char(file);
 
-        if (*c == '\0')
+        if (c == '\0')
         {
             break;
         }
@@ -146,7 +141,7 @@ void process_file(const char *file_path)
 
             is_in_word = 0;
         }
-        else if (!is_in_word && (is_vowel(c) || is_number(c) || is_consonant(c) || *c == '_'))
+        else if (!is_in_word && (is_vowel(c) || is_number(c) || is_consonant(c) || c == '_'))
         {
             if (is_vowel(c))
             {
@@ -157,15 +152,12 @@ void process_file(const char *file_path)
             words_number++;
         }
 
-        if (is_separator(c) || is_vowel(c) || is_number(c) || is_consonant(c) || *c == '_' || *c == '\'')
+        if (is_separator(c) || is_vowel(c) || is_number(c) || is_consonant(c) || c == '_' || c == '\'')
         {
-            memcpy(last_c, c, 4);
+            last_c = c;
         }
-
-        free(c);
     }
 
-    free(last_c);
     printf("File: %s\n", file_path);
     printf("Number of words: %d\n", words_number);
     printf("Number of words starting with vowels: %d\n", words_vowel_start_number);
