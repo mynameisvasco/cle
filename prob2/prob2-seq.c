920eaa32e09f94a4c9ff09f9c34472fc263bac36
Roberto <robertograca@ua.pt>
2022-04-09 20:04:46 +0100

    Determinants are calculated correctly

--- prob2/prob2-seq.c
+++ prob2/prob2-seq.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <time.h>
 
-int SIZE = 0;
+int SIZE = 3;
 
 // Swaps the values of columns X and Y
 void swap_columns(double (*matrix)[SIZE], int *x, int *y)
@@ -33,7 +33,7 @@ double process_matrix(double (*matrix)[SIZE], int *signal_reversion)
 
             for (int j = i + 1; j < SIZE; j++)
             {
-                if (matrix[i][j] != 0)
+                if (matrix[j][i] != 0)
                 {
                     swap_columns(matrix, &i, &j);
                     *signal_reversion = !(*signal_reversion);
@@ -41,13 +41,15 @@ double process_matrix(double (*matrix)[SIZE], int *signal_reversion)
                 }
             }
         }
-        for (int j = i; j < SIZE; j++)
+        for (int j = SIZE - 1; j > i - 1; j--)
         {
             for (int k = i + 1; k < SIZE; k++)
             {
                 transformation(&matrix[k][j], matrix[k][i], matrix[i][i], matrix[i][j]);
             }
         }
+        if (matrix[i][i] == 0)
+            return 0;
         determinant *= matrix[i][i];
     }
     return determinant;
@@ -66,79 +68,42 @@ void process_file(char *file_path)
     // reads the number of matrixes and the matrixes order (int = 4 bytes)
     int matrix_count, matrix_order;
     int signal_reversion = 0;
-    fread(&matrix_count, sizeof(int), 1, file);
-    fread(&matrix_order, sizeof(int), 1, file);
+    if (!fread(&matrix_count, sizeof(int), 1, file))
+    {
+        printf("Error reading number of matrices!");
+        exit(1);
+    }
+    if (!fread(&matrix_order, sizeof(int), 1, file))
+    {
+        printf("Error reading matrices order!");
+        exit(1);
+    }
 
     SIZE = matrix_order;
 
     printf("Number of matrices to be read: %d\n", matrix_count);
     printf("Matrices order: %d\n\n", SIZE);
 
-    // double matrix[5][5] = {{1, 2, 3, 4, 5}, {6, 7, 8, 9, 10}, {11, 12, 13, 14, 15}, {16, 17, 18, 19, 20}, {21, 22, 23, 24, 25}};
-
-    // printf("\nBefore Processing...\n");
-
-    // for (int i = 0; i < SIZE; i++)
-    // {
-    //     for (int j = 0; j < SIZE; j++)
-    //     {
-    //         printf("%.3f\t", matrix[i][j]);
-    //     }
-    //     printf("\n");
-    // }
-    // double det = process_matrix(matrix, &signal_reversion);
-
-    // printf("\nAfter Processing...\n");
-
-    // for (int i = 0; i < SIZE; i++)
-    // {
-    //     for (int j = 0; j < SIZE; j++)
-    //     {
-    //         printf("%.3f\t", matrix[i][j]);
-    //     }
-    //     printf("\n");
-    // }
-
-    // printf("\nThe determinant is %.3e\n", det);
-
     double matrix[SIZE][SIZE];
-    // int cont = 1;
-    // while (matrix_count-- > 0)
-    // {
-    // printf("Processing matrix %d\n", cont++);
-    for (int i = 0; i < matrix_order; i++)
+    int cont = 1;
+    while (matrix_count-- > 0)
     {
-        fread(&matrix[i], sizeof(double), matrix_order, file);
+        printf("Processing matrix %d\n", cont++);
+        for (int i = 0; i < matrix_order; i++)
+        {
+            if (!fread(&matrix[i], sizeof(double), matrix_order, file))
+            {
+                printf("Error reading values from matrix!");
+                exit(2);
+            }
+        }
+
+        double det = process_matrix(matrix, &signal_reversion);
+
+        if (signal_reversion)
+            det = -det;
+        printf("The determinant is %.3e\n", det);
     }
-
-    // printf("\nBefore Processing...\n");
-
-    // for (int i = 0; i < SIZE; i++)
-    // {
-    //     for (int j = 0; j < SIZE; j++)
-    //     {
-    //         printf("%.3f\t", matrix[i][j]);
-    //     }
-    //     printf("\n");
-    // }
-
-    double det = process_matrix(matrix, &signal_reversion);
-
-    // printf("\nAfter Processing...\n");
-
-    // for (int i = 0; i < SIZE; i++)
-    // {
-    //     for (int j = 0; j < SIZE; j++)
-    //     {
-    //         printf("%.3f\t", matrix[i][j]);
-    //     }
-    //     printf("\n");
-    // }
-
-    if (signal_reversion)
-        det = -det;
-    printf("The determinant is %.3e\n", det);
-    // }
 }
 
 int main(int argc, char **argv)
