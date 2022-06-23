e386e0403772222b6c95da69f2a5907614ad31fe
Vasco <jvcs@ua.pt>
2022-06-23 18:08:50 +0100

    Very nice working

--- cuda/prog_cols/prog_cols.cu
+++ cuda/prog_cols/prog_cols.cu
@@ -5,97 +5,47 @@
 #include "matrix.h"
 #include "common.h"
 
-void setup_device();
-
-int process_cmd(char **files_paths, int argc, char **argv);
-
-int read_matrices(double *matrix, char **files_paths, int files_n);
-
 __global__ static void process_matrices(double *matrix, double *results, int order);
 
-struct timespec start, finish;
-
+/*
+ * Main method of the program
+ *
+ * Arguments - Runtime arguments
+ */
 int main(int argc, char **argv)
 {
-    setup_device();
-    char *files_paths[10];
-    int files_n = process_cmd(files_paths, argc, argv);
-    double *matrices_host = (double *)malloc(sizeof(double) * 128 * 32 * 32);
-    double *results_host = (double *)malloc(sizeof(double) * 128);
+    struct timespec start, finish;
+    printf("%s Starting...\n", argv[0]);
 
-    for (int i = 0; i < 128; i++)
-    {
-        results_host[i] = 1;
-    }
-
-    double *matrices_device;
-    double *results_device;
-    int matrices_count = read_matrices(matrices_host, files_paths, files_n);
-    CHECK(cudaMalloc(&matrices_device, sizeof(double) * 128 * 32 * 32));
-    CHECK(cudaMemcpy(matrices_device, matrices_host, sizeof(double) * 128 * 32 * 32, cudaMemcpyHostToDevice));
-    CHECK(cudaMalloc(&results_device, sizeof(double) * 128));
-    CHECK(cudaMemcpy(results_device, results_host, sizeof(double) * 128, cudaMemcpyHostToDevice));
-    free(matrices_host);
-
-    dim3 grid, block;
-    grid.x = 128;
-    grid.y = 1;
-    grid.z = 1;
-    block.x = 32;
-    block.y = 1;
-    block.z = 1;
-
-    clock_gettime(CLOCK_MONOTONIC_RAW, &start);
-    double elapsed;
-    process_matrices<<<grid, block>>>(matrices_device, results_device, 32);
-    CHECK(cudaDeviceSynchronize());
-    CHECK(cudaGetLastError());
-
-    results_host = (double *)malloc(sizeof(double) * 128);
-    CHECK(cudaMemcpy(results_host, results_device, sizeof(double) * 128, cudaMemcpyDeviceToHost));
-
-    for (int i = 0; i < 128; i++)
-    {
-        printf("det(Matrix %d) = %.3e\n", i + 1, results_host[i]);
-    }
-
-    clock_gettime(CLOCK_MONOTONIC_RAW, &finish);
-    elapsed = (finish.tv_sec - start.tv_sec);
-    elapsed += (finish.tv_nsec - start.tv_nsec) / 1000000000.0;
-    printf("Elapsed time = %.5f s\n", elapsed);
-    free(results_host);
-}
-
-void setup_device()
-{
+    // set up device
     int dev = 0;
-    int i;
-
     cudaDeviceProp deviceProp;
     CHECK(cudaGetDeviceProperties(&deviceProp, dev));
     printf("Using Device %d: %s\n", dev, deviceProp.name);
     CHECK(cudaSetDevice(dev));
-}
 
-int process_cmd(char **files_paths, int argc, char **argv)
-{
+    // declare storage variables
     int files_n = 0;
+    char *files_paths[10] = {0};
+    double *results;
+    double *results_gpu;
+    double *matrix;
+    double *matrix_gpu;
     int input = 0;
+    double elapsed;
 
+    // parse command line arguments
     while (input != -1)
     {
         input = getopt(argc, argv, "f:");
+
         if (input == 'f')
         {
             files_paths[files_n++] = optarg;
         }
     }
 
-    return files_n;
-}
-
-int read_matrices(double *matrices, char **files_paths, int files_n)
-{
+    // start timer
     for (int file_index = 0; file_index < files_n; file_index++)
     {
         FILE *file = fopen(files_paths[file_index], "rb");
@@ -106,6 +56,7 @@ int read_matrices(double *matrices, char **files_paths, int files_n)
             exit(1);
         }
 
+        // get the number o f matrices to process and their order
         int matrix_count, matrix_order;
 
         if (fread(&matrix_count, sizeof(int), 1, file) != 1)
@@ -120,14 +71,82 @@ int read_matrices(double *matrices, char **files_paths, int files_n)
             exit(1);
         }
 
-        int num_read = fread(matrices, sizeof(double), matrix_count * matrix_order * matrix_order, file);
+        // allocate space on the Host for the matrices
+        matrix = (double *)malloc(sizeof(double) * matrix_count * matrix_order * matrix_order);
+        // read the remaining contents of the file (all the matrices coefficients)
 
-        if (num_read != matrix_count * matrix_order * matrix_order)
+        int num_read = fread(matrix, sizeof(double), matrix_order * matrix_order * matrix_count, file);
+
+        if (num_read != (matrix_order * matrix_order * matrix_count))
         {
             perror("Error reading values from matrix!\n");
             exit(2);
         }
+
+        // prepare launching grid
+        dim3 grid, block;
+        grid.x = matrix_count;
+        grid.y = 1;
+        grid.z = 1;
+        block.x = matrix_order;
+        block.y = 1;
+        block.z = 1;
+
+        // allocate space for Host results
+        results = (double *)malloc(sizeof(double) * matrix_count);
+
+        // initialize Host results with 1
+        for (int i = 0; i < matrix_count; i++)
+        {
+            results[i] = 1.0;
+        }
+
+        // allocate space on the Device for the matrices and the results
+        CHECK(cudaMalloc(&results_gpu, sizeof(double) * matrix_count));
+        CHECK(cudaMalloc(&matrix_gpu, sizeof(double) * matrix_count * matrix_order * matrix_order));
+
+        // copy matrices and results to the Device
+        CHECK(cudaMemcpy(matrix_gpu, matrix, sizeof(double) * matrix_count * matrix_order * matrix_order, cudaMemcpyHostToDevice));
+        CHECK(cudaMemcpy(results_gpu, results, sizeof(double) * matrix_count, cudaMemcpyHostToDevice));
+        // free matrices on Host
+        free(matrix);
+
+        clock_gettime(CLOCK_MONOTONIC_RAW, &start);
+        // process file on GPU
+        process_matrices<<<grid, block>>>(matrix_gpu, results_gpu, matrix_order);
+
+        // wait for processing to finish
+        CHECK(cudaDeviceSynchronize());
+
+        // stop timer
+        clock_gettime(CLOCK_MONOTONIC_RAW, &finish);
+        elapsed = (finish.tv_sec - start.tv_sec);
+        elapsed += (finish.tv_nsec - start.tv_nsec) / 1000000000.0;
+
+        // free matrices on Device
+        CHECK(cudaFree(matrix_gpu));
+
+        // copy results to Host
+        results = (double *)malloc(sizeof(double) * 128);
+        CHECK(cudaMemcpy(results, results_gpu, sizeof(double) * matrix_count, cudaMemcpyDeviceToHost));
+
+        // free results on Device
+        CHECK(cudaFree(results_gpu));
+
+        // print results
+        printf("\n\nProcessing file: %s\n", files_paths[file_index]);
+        printf("Number of matrices to be read: %d\n", matrix_count);
+        printf("Matrices order: %d\n\n", matrix_order);
+
+        for (int matrix_index = 0; matrix_index < matrix_count; matrix_index++)
+        {
+            printf("det(Matrix %d) = %.3e\n", matrix_index + 1, results[matrix_index]);
+        }
+
+        free(results);
     }
+
+    printf("Elapsed time = %.5f s\n", elapsed);
 }
 
 __global__ static void process_matrices(double *matrix, double *results, int order)
@@ -139,57 +158,33 @@ __global__ static void process_matrices(double *matrix, double *results, int ord
     int row_id = threadIdx.x + blockDim.x * threadIdx.y + blockDim.x * blockDim.y * threadIdx.z;
 
     // Start of the matrix
-    double *mat = &matrix[matrix_id * order * order];
+    int mat = matrix_id * order * order;
 
-    // Start of the row
-    double *row = &mat[row_id * order];
-    double *ii = row + row_id;
+    // Start of row
+    int row = mat + row_id * order;
+
     unsigned int signal_reversion = 0;
-    // If position (i,i) is equal to 0 then look for a column different from 0 and swap
 
     for (int i = 0; i < order; i++)
     {
-        if (row_id > i)
+        int current_row = mat + i * order;
+
+        if (row_id < i)
         {
             continue;
         }
 
-        if (*ii == 0)
+        if (threadIdx.x == i)
         {
-            for (int j = i + 1; j < order; j++)
-            {
-                if (mat[order * i + j] != 0)
-                {
-                    for (int k = 0; j < order; k++)
-                    {
-                        double tmp = mat[order * k + i];
-                        mat[order * k + i] = matrix[order * k + j];
-                        mat[order * k + j] = tmp;
-                    }
-
-                    signal_reversion = !signal_reversion;
-                    break;
-                }
-            }
+            results[matrix_id] *= matrix[current_row + i];
+            continue;
         }
 
-        // Apply the Guassian transformation
-        for (int j = order - 1; j > row_id - 1; j--)
+        for (int j = i + 1; j < order; j++)
         {
-            for (int k = row_id + 1; k < order; k++)
-            {
-                mat[order * k + j] = mat[order * k + j] - ((mat[order * k + row_id] / *ii) * mat[order * row_id + j]);
-            }
+            matrix[row + j] -= matrix[current_row + j] * matrix[row + i] / matrix[current_row + i];
         }
 
         __syncthreads();
-    }
-
-    if (threadIdx.x % order == 0)
-    {
-        for (int i = 0; i < order; i++)
-        {
-            results[matrix_id] *= mat[i * order + i];
-        }
     }
 }
